e5da727ce047deb175231bd3ff80fe24469c4cae
Gexilable <zilelol4@gmail.com>
2023-02-02 22:24:22 +0300

    add convert view

--- app/views.py
+++ app/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+import io
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import FileResponse, Http404
+
+
+
 def index(request):
     return render(request, "app/index.html")
+
+
+def convert(request):
+    try:
+        return FileResponse(io.BytesIO(b"123"), content_type="application/pdf")
+    except FileNotFoundError:
+        raise Http404()
