4475e3cb1afd60463d71ef0797da19227c348d8a
Gexilable <zilelol4@gmail.com>
2023-02-09 22:32:44 +0300

    create GeneratedFile model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class GeneratedFile(models.Model):
+    filename = models.CharField(max_length=256)
+    value = models.BinaryField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
